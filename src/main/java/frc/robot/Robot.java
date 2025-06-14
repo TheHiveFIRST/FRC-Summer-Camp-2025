3a741b31838e3c1e887c61f12605ed340f3a4380
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2025-06-14 14:48:09 -0600

    Probably works.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,86 +1,53 @@
-// Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.XboxController;
-
+import edu.wpi.first.util.sendable.SendableRegistry;
+import com.revrobotics.spark.SparkBase.PersistMode; // This one still seems to be directly under SparkBase for now based on docs/examples
+import com.revrobotics.spark.SparkBase.ResetMode; // This one still seems to be directly under SparkBase for now based on docs/examples
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 // NEW IMPORTS FOR REVLIB 2025 - CORRECTED PATHS FOR ENUMS
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.config.SparkMaxConfig;
-
 // Corrected import paths for IdleMode, ResetMode, PersistMode
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode; // Corrected path
-import com.revrobotics.spark.SparkBase.ResetMode; // This one still seems to be directly under SparkBase for now based on docs/examples
-import com.revrobotics.spark.SparkBase.PersistMode; // This one still seems to be directly under SparkBase for now based on docs/examples
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
- * the code necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
+  private final SparkMax m_leftMotor1 = new SparkMax(1, MotorType.kBrushed);
+  private final SparkMax m_leftMotor2 = new SparkMax(2, MotorType.kBrushed);
+  private final SparkMax m_rightMotor3 = new SparkMax(3, MotorType.kBrushed);
+  private final SparkMax m_rightMotor4 = new SparkMax(4, MotorType.kBrushed);
+
+  private final SparkMaxConfig leftConfig = new SparkMaxConfig();
+  private final SparkMaxConfig rightConfig = new SparkMaxConfig();
+
   private final DifferentialDrive m_robotDrive;
   private final XboxController m_controller;
 
-  // Change to SparkMax (no CAN prefix)
-  private final SparkMax m_leftLeader = new SparkMax(1, MotorType.kBrushed);
-  private final SparkMax m_leftFollower = new SparkMax(2, MotorType.kBrushed);
-  private final SparkMax m_rightLeader = new SparkMax(3, MotorType.kBrushed);
-  private final SparkMax m_rightFollower = new SparkMax(4, MotorType.kBrushed);
-
   /** Called once at the beginning of the robot program. */
   public Robot() {
-    // The DifferentialDrive class expects individual motor controllers.
-    // We pass the leader motors to DifferentialDrive, and the followers will
-    // automatically match their leader's output due to the .follow() configuration.
-    m_robotDrive = new DifferentialDrive(m_leftLeader, m_rightLeader);
+    leftConfig.follow(2, false);
+    rightConfig.follow(4, false);
 
-    // Optional: Set a safety expiration for the motors. If motor updates stop,
-    // motors will be disabled after this time (in seconds).
-    m_robotDrive.setSafetyEnabled(true);
-    m_robotDrive.setExpiration(0.1); // Default is 0.1 seconds, can be adjusted
+    m_leftMotor1.configure(leftConfig, null, null);
+    m_rightMotor3.configure(rightConfig, null, null);
 
+    m_robotDrive = new DifferentialDrive(m_leftMotor2::set, m_rightMotor4::set);
     m_controller = new XboxController(0);
+
+    SendableRegistry.addChild(m_robotDrive, m_leftMotor2);
+    SendableRegistry.addChild(m_robotDrive, m_rightMotor4);
   }
 
   @Override
   public void teleopPeriodic() {
-    // Drive the robot using tank drive with Xbox controller Y-axes
-    m_robotDrive.tankDrive(m_controller.getLeftY(), m_controller.getRightY());
-  }
-
-  @Override
-  public void robotInit() {
-    // --- NEW REVLIB 2025 CONFIGURATION SYSTEM ---
-
-    // Create configuration objects
-    SparkMaxConfig leftLeaderConfig = new SparkMaxConfig();
-    SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
-    SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
-    SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
-
-    // Configure follower motors
-    leftFollowerConfig.follow(m_leftLeader.getDeviceId(), false); // false for not inverted
-    rightFollowerConfig.follow(m_rightLeader.getDeviceId(), true); // true for inverted, as right side needs inversion
-
-    // Invert the right side leader motor
-    rightLeaderConfig.inverted(true); // Invert the leader
-
-    // Example of setting IdleMode (e.g., to Coast)
-    leftLeaderConfig.idleMode(IdleMode.kCoast);
-    leftFollowerConfig.idleMode(IdleMode.kCoast);
-    rightLeaderConfig.idleMode(IdleMode.kCoast);
-    rightFollowerConfig.idleMode(IdleMode.kCoast);
-
-    // Apply configurations to the motors
-    m_leftLeader.configure(leftLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    m_rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
+    // Steer with left stick, throttle with triggers.
+    // Right trigger moves forward, left trigger moves backward.
+    m_robotDrive.arcadeDrive(-m_controller.getLeftX(), m_controller.getRightTriggerAxis() - m_controller.getLeftTriggerAxis());
+    }
 }
